4bc2baa040d9e1d5ac289919c4d8dcc87d8dd060
Adam Manhercz <manhercz.adam@gmail.com>
2023-04-06 15:35:47 +0200

    Adjustments

--- librarian.py
+++ librarian.py
@@ -5,6 +5,7 @@ Executes operations attached to the Library.
 
 """
 
+from typing import List
 from library import Library
 from student import Student
 from book import Book
@@ -15,7 +16,6 @@ class Librarian:
     """Deals with operations between students and library"""
 
     def __init__(self, library: Library) -> None:
-
         self.library = library
 
     def registrate(self, student: Student):
@@ -28,39 +28,35 @@ class Librarian:
         print("{} is registrated!".format(student.student_name))
         print("========================")
 
-    def add_book(self, book: Book) -> None:
+    def add_book(self, book: Book) -> str:
         """Adds books to the library"""
 
         self.library.collection.append(book)
-        print(f"The book {book.name} has been successfully added to the library!")
+        print("The book {book.name} has been successfully added to the library!")
 
-    def remove_book(self, book: Book) -> None:
+    def remove_book(self, book: Book) -> str:
         """Removes books from the library"""
 
         self.library.collection.remove(book)
-        print(f"The book {book} has been removed from the library!")
+        return f"The book {book} has been removed from the library!"
 
-    def print_student(self, student: Student):
+    def print_student(self, student: Student) -> str:
         """Prints the chosen, registrated student's data"""
 
         if student.account != None:
-            print(
-                "Name: {}\nClass: {}\nBalance: ${}\nLoaned books: {}".format(
-                    student.student_name,
-                    student.year,
-                    student.account.balance,
-                    student.account.loaned_books,
-                )
-            )
+            print(f"Name: {student.student_name}")
+            print(f"Class: {student.year}")
+            print(f"Balance: ${student.account.balance}")
+            print(f"Loaned books: {student.account.loaned_books}")
         else:
             print(f"{student.student_name} is not registrated yet.")
 
-    def print_collection(self):
+    def print_collection(self) -> str:
         """Prints the collection of the library"""
 
         print(self.library.collection)
 
-    def print_registrated(self):
+    def print_registrated(self) -> str:
         """Prints the registrated students"""
 
         print(self.library.accounts)
